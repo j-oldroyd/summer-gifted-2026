f0c841c3f5d199329b1886ae0406b9d4a4f53fdf
Jesse Oldroyd <oldroyd.j@wvwc.edu>
2026-06-30 11:56:56 -0400

    Initial commits for adjacency_matrix.py and gale_shapley_algorithm.py

---
+++ adjacency_matrix.py
@@ -0,0 +1,22 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+A = np.array(
+    [
+        [0, 1, 0, 1, 1, 0, 0],
+        [1, 0, 0, 1, 1, 0, 1],
+        [0, 0, 0, 1, 0, 0, 1],
+        [1, 1, 1, 0, 1, 0, 0],
+        [1, 1, 0, 1, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 0, 1, 1, 0],
+    ]
+)
+
+G = nx.from_numpy_array(A)
+
+nx.display(G)
+
+plt.show()
+plt.clf()
---
+++ gale_shapley_algorithm.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+applicants = ['1', '2', '3', '4']
+employers = ['A', 'B', 'C', 'D']
