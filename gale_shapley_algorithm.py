29bbf485784a4858cbc7d271860dac3fe0c094e4
Jesse Oldroyd <oldroyd.j@wvwc.edu>
2026-06-30 12:49:54 -0400

    Implement algorithm

--- gale_shapley_algorithm.py
+++ gale_shapley_algorithm.py
@@ -1,4 +1,33 @@
 import numpy as np
 
-applicants = ['1', '2', '3', '4']
-employers = ['A', 'B', 'C', 'D']
+employer_prefs = {
+    "A": ["2", "1", "3", "4"],
+    "B": ["4", "1", "2", "3"],
+    "C": ["1", "3", "2", "4"],
+    "D": ["2", "3", "1", "4"],
+}
+
+applicant_prefs = {
+    "1": ["A", "C", "B", "D"],
+    "2": ["C", "D", "A", "B"],
+    "3": ["D", "B", "C", "A"],
+    "4": ["C", "B", "A", "D"],
+}
+
+unfilled_positions = list(employer_prefs.keys())
+filled_positions = {}
+
+while unfilled_positions:
+    job = unfilled_positions.pop(0) # get first unfilled position
+    prefs = employer_prefs[job] # get applicant preferences for that position
+
+    for applicant in prefs:
+        current_applicant_prefs = applicant_prefs[applicant]
+        current_job = filled_positions.get(applicant)
+        if current_job is None:
+            filled_positions[applicant] = job
+            break
+        elif current_applicant_prefs.index(current_job) > current_applicant_prefs.index(job):
+            filled_positions[applicant] = job
+            unfilled_positions.append(current_job)
+            break
